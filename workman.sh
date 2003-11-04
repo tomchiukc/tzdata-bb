3cc2bab1b0707cba1409355c2d116d33fae3fda7
Arthur David Olson <olsona@elsie>
2003-11-04 09:35:31 -0500

    SCCS-file: workman.sh SCCS-SID: 1.2

--- workman.sh
+++ workman.sh
@@ -1,32 +1,40 @@
 #! /bin/sh
 
-# This file is in the public domain, so clarified as of
-# 2009-05-17 by Arthur David Olson.
+# %W%
 
-# Tell groff not to emit SGR escape sequences (ANSI color escapes).
-GROFF_NO_SGR=1
-export GROFF_NO_SGR
+tty -s
+ttyval=$?
 
-echo ".am TH
-.hy 0
-.na
-..
-.rm }H
-.rm }F" | nroff -man - ${1+"$@"} | perl -ne '
-	binmode STDIN, '\'':encoding(utf8)'\'';
-	binmode STDOUT, '\'':encoding(utf8)'\'';
+case $# in
+	0)	nroff -man ;;
+	1)	if [ -f $1 ]
+		then
+			( echo .hy 0; echo .na ) | nroff -man - "$1"
+		else
+			man "$1"
+		fi ;;
+	*)	man ${1+"$@"} ;;
+esac | perl -ne '
+	if (($. % 66) <= 7) {
+		next;
+	}
+	if (($. % 66) > (66 - 7)) {
+		next;
+	}
 	chomp;
-	s/.\010//g;
-	s/\s*$//;
+	s/.//g;
+	s/[ 	]*$//;
 	if (/^$/) {
 		$sawblank = 1;
 		next;
 	} else {
-		if ($sawblank && $didprint) {
+		if ($sawblank) {
 			print "\n";
 			$sawblank = 0;
 		}
 		print "$_\n";
-		$didprint = 1;
 	}
-'
+' | case $ttyval in
+	0)	more ;;
+	*)	cat ;;
+esac
