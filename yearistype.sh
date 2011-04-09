27fd3a7568af0c02818e2a3a033fd26a1584af5b
Ted Percival <ted@midg3t.net>
2011-04-08 22:25:45 -0600

    Imported Upstream version 2008b

--- yearistype.sh
+++ yearistype.sh
@@ -3,9 +3,11 @@
 : 'This file is in the public domain, so clarified as of'
 : '2006-07-17 by Arthur David Olson.'
 
+: '@(#)yearistype.sh	8.2'
+
 case $#-$1 in
 	2-|2-0*|2-*[!0-9]*)
-		echo "$0: wild year: $1" >&2
+		echo "$0: wild year - $1" >&2
 		exit 1 ;;
 esac
 
@@ -29,5 +31,5 @@ case $#-$2 in
 			*)				exit 1 ;;
 		esac ;;
 	2-*)
-		echo "$0: wild type: $2" >&2 ;;
+		echo "$0: wild type - $2" >&2 ;;
 esac
