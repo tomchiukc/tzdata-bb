0e035e3a8003b250303ce53bad72a27dc16da3ac
Paul Eggert <eggert@cs.ucla.edu>
2014-06-15 17:38:04 -0700

    Simplify use of URLs in text files.
    In several cases our text files had commentary like this:
    # Egypt to cancel daylight saving time
    # <a href="http://www.almasryalyoum.com/en/node/407168">
    # http://www.almasryalyoum.com/en/node/407168
    # </a>
    where lines 2 and 4 let the file be viewed by a browser with the
    URL being clickable.  In practice this approach does not work with
    many modern browsers (I tested with Firefox and with Chrome) and
    it clutters up the data, so in cases like the above this patch
    removes lines 2 and 4.
    (Problem reported by Steffen "Daode" Nurpmeso.)

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 # <pre>
 # %W%
+=======
+>>>>>>> Simplify use of URLs in text files.
 # This file is in the public domain, so clarified as of
 # 2009-05-17 by Arthur David Olson.
 
--- workman.sh
+++ workman.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
+<<<<<<< HEAD
 # <pre>
 # %W%
+=======
+>>>>>>> Simplify use of URLs in text files.
 # This file is in the public domain, so clarified as of
 # 2009-05-17 by Arthur David Olson.
 
